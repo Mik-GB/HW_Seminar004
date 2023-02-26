4de4140f7ebf1fec48e668951fb8b58ae96f4fdd
Mike Pronin <mr.mi@mail.ru>
2023-02-26 16:09:57 +0400

    дз задача 27

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -2,3 +2,22 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Введите число");
+if (!int.TryParse(Console.ReadLine(), out int number))
+{
+  Console.WriteLine("Не является числом");
+  return;
+}
+// if (number < 0) number *= -1;
+int SummNumber(int number)
+{
+  int SummNumber = 0;
+  while (number > 0)
+  {
+    SummNumber = SummNumber + number % 10;
+    number = number / 10;
+  }
+  return SummNumber;
+}
+Console.WriteLine($"Сумма цифр числа: {SummNumber(number)}");
