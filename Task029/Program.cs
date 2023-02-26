a6274848cd18784c29741c5168e2bab8c3f57dd8
Mike Pronin <mr.mi@mail.ru>
2023-02-26 17:03:55 +0400

    дз задача 29

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size]; 
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1000);
+    }
+    return array;
+}
+
+Console.WriteLine($"Массив: [ {String.Join("; ", GetArray(8))} ]");
