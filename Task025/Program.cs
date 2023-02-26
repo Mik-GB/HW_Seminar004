bf733035b059296dfe545b54447befb17866ba3c
Mike Pronin <mr.mi@mail.ru>
2023-02-26 15:40:49 +0400

    дз задача 25

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -2,19 +2,49 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число A: ");
-//int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число A: ");
+// //int A = Convert.ToInt32(Console.ReadLine());
+// if (!int.TryParse(Console.ReadLine(), out int A))
+// {
+//   Console.WriteLine("А не является числом");
+//   return;
+// }
+// Console.Write("Введите число B: ");
+// //int B = Convert.ToInt32(Console.ReadLine());
+// if (!int.TryParse(Console.ReadLine(), out int B))
+// {
+//   Console.WriteLine("B не является числом");
+//   return;
+// }
+
+// Console.WriteLine(Math.Pow(A, B));
+
+Console.WriteLine("Введите число A");
 if (!int.TryParse(Console.ReadLine(), out int A))
 {
   Console.WriteLine("А не является числом");
   return;
 }
-Console.Write("Введите число B: ");
-//int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
 if (!int.TryParse(Console.ReadLine(), out int B))
 {
   Console.WriteLine("B не является числом");
   return;
 }
-
-Console.WriteLine(Math.Pow(A, B));
+if (B < 0)
+{
+  Console.WriteLine("Неверное значение числа В");
+  return;
+}
+int Natur(int A, int B)
+{
+  int result = 1;
+  if (B > 0)
+    for (int i = 1; i <= B; i++)
+    {
+      result = result * A;
+    }
+  return result;
+}
+// Console.WriteLine(Natur(3, 5)); 
+Console.WriteLine($"число {A} в натуральной степени {B} = {Natur(A, B)}");
